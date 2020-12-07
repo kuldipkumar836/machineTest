0eafc729121684201a62ccc121d0853f68a661ad
unknown <techiegram836@gmail.com>
2020-12-08 01:23:00 +0530

    added header footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,15 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { BlogComponent } from './blog/blog.component';
 import { ContentComponent } from './blog/content/content.component';
+import { HeaderComponent } from './blog/header/header.component';
+import { FooterComponent } from './blog/footer/footer.component';
 @NgModule({
   declarations: [
     AppComponent,
     BlogComponent,
     ContentComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
